870cc2f5043d61852d4fa4a4cb4ef33c0f809f0e
David Lisk <david.lisk@xsresearch.co.uk>
2017-12-17 23:00:56 +0000

    Set plot type to png
    Signed-off-by: David Lisk <david.lisk@xsresearch.co.uk>

--- plot.r
+++ plot.r
@@ -2,6 +2,8 @@
 # Requires data to be in the same format as spiral
 #   - two variables and three classes
 
+png('plot.png')
+
 data_orig = read.csv('spiral.csv', header=F)
 colnames(data_orig) <- c('t1','t2','t3','d1','d2')
 data = read.csv('test_output.csv', header=F)
@@ -11,3 +13,5 @@ data$result = apply(data[c('t1','t2','t3')],1,which.max)
 colfn <- function(n){ ifelse(n==1,'red',ifelse(n==2,'green','blue')) }
 plot(data[c('d1','d2')], col=colfn(data['result']))
 points(data_orig[,c('d1','d2')])
+
+dev.off()
